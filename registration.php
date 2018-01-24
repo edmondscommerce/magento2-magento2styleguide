4eac8fe4226c7306a1d7983cae486e00a6144eb7
Ken <ken@edmondscommerce.co.uk>
2018-01-24 15:37:22 +0000

    Added boostrap M2 module files

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'EdmondsCommerce_Magento2StyleGuide',
+    __DIR__
+);
