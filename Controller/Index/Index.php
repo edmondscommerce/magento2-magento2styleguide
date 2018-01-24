ef33e7a504bb452c567e2f2d78a3bac0ef1a1bfc
Ken <ken@edmondscommerce.co.uk>
2018-01-24 19:09:18 +0000

    Populated the page with various elements

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -3,12 +3,26 @@
 namespace EdmondsCommerce\Magento2StyleGuide\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 
 class Index extends Action
 {
+    protected $messageManager;
+
+    public function __construct(Context $context, \Magento\Framework\Message\ManagerInterface $messageManager)
+    {
+        $this->messageManager = $messageManager;
+        parent::__construct($context);
+    }
+
     public function execute()
     {
+        $this->messageManager->addSuccessMessage("Success");
+        $this->messageManager->addErrorMessage("Error");
+        $this->messageManager->addWarningMessage("Warning");
+        $this->messageManager->addNoticeMessage("Notice");
+
         $page = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
         return $page;
     }
